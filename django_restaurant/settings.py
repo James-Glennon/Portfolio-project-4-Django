da22d18b2cb261bf522bcadc2c82d3f4e42a6fc3
James-Glennon <>
2023-07-13 03:44:35 +0000

    heroku troubleshooting (4)

--- django_restaurant/settings.py
+++ django_restaurant/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-james-glennon-portfolio-v6rvjsr8oi.us2.codeanyapp.com',
                  'portfolio4-django-78b028d88d82.herokuapp.com']
